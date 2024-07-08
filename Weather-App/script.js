21937e229d4f164da5579a4b6f612e1fd057905f
Ramtej9989 <tejbonthu45@gmail.com>
2024-07-08 12:50:59 +0530

    Add files via upload

---
+++ Weather-App/script.js
@@ -0,0 +1,99 @@
+const input = document.getElementById("search-input");
+const description = document.getElementById("description-text");
+const img = document.getElementById("description-img");
+const locBtn = document.getElementById("loc-btn");
+const section = document.querySelector("section");
+const form = document.querySelector("form");
+
+function getData(e) {
+  e.preventDefault();
+
+  if (!input.value) {
+    alert("Please Enter a city name");
+    return;
+  } else {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=ca695dcbc66c5fa3d0cb955033fd918f`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        displayWeather(data);
+        document.getElementById("city").style.display = "block";
+      });
+  }
+}
+
+function getLocationData() {
+  if (!navigator.geolocation) {
+    alert("geolocation is not supported!");
+    return;
+  } else {
+    navigator.geolocation.getCurrentPosition((position) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=ca695dcbc66c5fa3d0cb955033fd918f`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          // console.log(data);
+          displayWeather(data);
+          document.querySelector("header h5").style.display = "none";
+          document.getElementById("city").textContent = "current location";
+        });
+    });
+  }
+}
+
+addEventListener("load", () => {
+  const date = document.getElementById("date");
+
+  const d = new Date();
+  let currentDate = d.toString().slice(4, 15);
+
+  date.innerHTML = currentDate;
+});
+
+function displayWeather(data) {
+  document.querySelector("header h5").style.display = "block";
+
+  const temp = (data.main.temp - 273.15).toFixed(1);
+  console.log(temp);
+
+  document.getElementById("temperature-degree").textContent = temp + "°";
+  document.getElementById("city").textContent = input.value;
+
+  document.getElementById("humidity-degree").textContent =
+    data.main.humidity + " %";
+  document.getElementById("feelslike-degree").textContent =
+    (data.main.feels_like - 273.15).toFixed(1) + " °";
+
+  description.textContent = data.weather[0].description;
+
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+  section.style.display = "block";
+  section.classList.add("test");
+  document.getElementById("city").style.display = "block";
+  locBtn.style.display = "none";
+
+  // console.log(document.getElementById("city"));
+}
+
+form.addEventListener("submit", getData);
+const dayElements = document.querySelectorAll('.day-name');
+const tempElements = document.querySelectorAll('.day-temp');
+const iconElements = document.querySelectorAll('.day-icon');
+
+dayElements.forEach((dayElement, index) => {
+    const day = Object.keys(dailyForecast)[index];
+    const data = dailyForecast[day];
+    dayElement.textContent = day;
+    tempElements[index].textContent = `${Math.round(data.minTemp)}º / ${Math.round(data.maxTemp)}º`;
+    iconElements[index].innerHTML = getWeatherIcon(data.icon);
+});
+
+function getWeatherIcon(iconCode) {
+    const iconBaseUrl = 'https://openweathermap.org/img/wn/';
+    const iconSize = '@2x.png';
+    return `<img src="${iconBaseUrl}${iconCode}${iconSize}" alt="Weather Icon">`;
+}
